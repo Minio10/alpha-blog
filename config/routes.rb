1219079b9ec7b4a70c92c03a08ecc774308397bf
Minio10 <herminiom10@gmail.com>
2022-04-25 14:59:17 +0100

    add password authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles, only: [:show, :index]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
